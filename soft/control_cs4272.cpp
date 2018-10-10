29fd641078511fcd18b08acbeceb07b5600c7457
mxmxmx <mxxstadler@gmail.com>
2018-10-10 07:35:56 +0200

    [reset pin]

--- soft/control_cs4272.cpp
+++ soft/control_cs4272.cpp
@@ -29,69 +29,70 @@
 
 #include <Arduino.h>
 #include "control_cs4272.h"
-#include "Wire.h"
+#include "i2c_t3.h"
 
-#define CS4272_ADDR 0x10 // TODO: need to double check
+#define CS4272_ADDR 0x10
 
 // Section 8.1 Mode Control
-#define CS4272_MODE_CONTROL			(uint8_t)0x01
+#define CS4272_MODE_CONTROL				(uint8_t)0x01
 #define CS4272_MC_FUNC_MODE(x)			(uint8_t)(((x) & 0x03) << 6)
 #define CS4272_MC_RATIO_SEL(x)			(uint8_t)(((x) & 0x03) << 4)
-#define CS4272_MC_MASTER_SLAVE			(uint8_t)0x08
+#define CS4272_MC_MASTER_SLAVE			(uint8_t)0x00 // (uint8_t)0x08 // 0x08 = Master
 #define CS4272_MC_SERIAL_FORMAT(x)		(uint8_t)(((x) & 0x07) << 0)
 
 // Section 8.2 DAC Control
-#define CS4272_DAC_CONTROL			(uint8_t)0x02
+#define CS4272_DAC_CONTROL				(uint8_t)0x02
 #define CS4272_DAC_CTRL_AUTO_MUTE		(uint8_t)0x80
 #define CS4272_DAC_CTRL_FILTER_SEL		(uint8_t)0x40
-#define CS4272_DAC_CTRL_DE_EMPHASIS(x)		(uint8_t)(((x) & 0x03) << 4)
+#define CS4272_DAC_CTRL_DE_EMPHASIS(x)	(uint8_t)(((x) & 0x03) << 4)
 #define CS4272_DAC_CTRL_VOL_RAMP_UP		(uint8_t)0x08
 #define CS4272_DAC_CTRL_VOL_RAMP_DN		(uint8_t)0x04
 #define CS4272_DAC_CTRL_INV_POL(x)		(uint8_t)(((x) & 0x03) << 0)
 
 // Section 8.3 DAC Volume and Mixing
-#define CS4272_DAC_VOL				(uint8_t)0x03
-#define CS4272_DAC_VOL_CH_VOL_TRACKING		(uint8_t)0x40
+#define CS4272_DAC_VOL					(uint8_t)0x03
+#define CS4272_DAC_VOL_CH_VOL_TRACKING	(uint8_t)0x40
 #define CS4272_DAC_VOL_SOFT_RAMP(x)		(uint8_t)(((x) & 0x03) << 4)
 #define CS4272_DAC_VOL_ATAPI(x)			(uint8_t)(((x) & 0x0F) << 0)
 
 // Section 8.4 DAC Channel A volume
-#define CS4272_DAC_CHA_VOL			(uint8_t)0x04
+#define CS4272_DAC_CHA_VOL				(uint8_t)0x04
 #define CS4272_DAC_CHA_VOL_MUTE			(uint8_t)0x80
-#define CS4272_DAC_CHA_VOL_VOLUME(x)		(uint8_t)(((x) & 0x7F) << 0)
+#define CS4272_DAC_CHA_VOL_VOLUME(x)	(uint8_t)(((x) & 0x7F) << 0)
 
 // Section 8.5 DAC Channel B volume
-#define CS4272_DAC_CHB_VOL			(uint8_t)0x05
+#define CS4272_DAC_CHB_VOL				(uint8_t)0x05
 #define CS4272_DAC_CHB_VOL_MUTE			(uint8_t)0x80
-#define CS4272_DAC_CHB_VOL_VOLUME(x)		(uint8_t)(((x) & 0x7F) << 0)
+#define CS4272_DAC_CHB_VOL_VOLUME(x)	(uint8_t)(((x) & 0x7F) << 0)
 
 // Section 8.6 ADC Control
-#define CS4272_ADC_CTRL				(uint8_t)0x06
+#define CS4272_ADC_CTRL					(uint8_t)0x06
 #define CS4272_ADC_CTRL_DITHER			(uint8_t)0x20
 #define CS4272_ADC_CTRL_SER_FORMAT		(uint8_t)0x10
 #define CS4272_ADC_CTRL_MUTE(x)			(uint8_t)(((x) & 0x03) << 2)
 #define CS4272_ADC_CTRL_HPF(x)			(uint8_t)(((x) & 0x03) << 0)
 
 // Section 8.7 Mode Control 2
-#define CS4272_MODE_CTRL2			(uint8_t)0x07
+#define CS4272_MODE_CTRL2				(uint8_t)0x07
 #define CS4272_MODE_CTRL2_LOOP			(uint8_t)0x10
-#define CS4272_MODE_CTRL2_MUTE_TRACK		(uint8_t)0x08
-#define CS4272_MODE_CTRL2_CTRL_FREEZE		(uint8_t)0x04
-#define CS4272_MODE_CTRL2_CTRL_PORT_EN		(uint8_t)0x02
-#define CS4272_MODE_CTRL2_POWER_DOWN		(uint8_t)0x01
+#define CS4272_MODE_CTRL2_MUTE_TRACK	(uint8_t)0x08
+#define CS4272_MODE_CTRL2_CTRL_FREEZE	(uint8_t)0x04
+#define CS4272_MODE_CTRL2_CTRL_PORT_EN	(uint8_t)0x02
+#define CS4272_MODE_CTRL2_POWER_DOWN	(uint8_t)0x01
 
 // Section 8.8 Chip ID
-#define CS4272_CHIP_ID				(uint8_t)0x08
+#define CS4272_CHIP_ID					(uint8_t)0x08
 #define CS4272_CHIP_ID_PART(x)			(uint8_t)(((x) & 0x0F) << 4)
 #define CS4272_CHIP_ID_REV(x)			(uint8_t)(((x) & 0x0F) << 0)
 
-
-#define CS4272_RESET_PIN 2
+// using different reset pin ...
+#define CS4272_RESET_PIN 10
 
 bool AudioControlCS4272::enable(void)
 {
-	Wire.begin();
 	delay(5);
+	Wire1.begin();
+	delay(25);
 	initLocalRegs();
 
 	// Setup Reset pin
@@ -114,7 +115,7 @@ bool AudioControlCS4272::enable(void)
 	delay(1);
 
 	// Set ratio select for MCLK=512*LRCLK (BCLK = 64*LRCLK), and master mode
-	write(CS4272_MODE_CONTROL, CS4272_MC_RATIO_SEL(3) | CS4272_MC_MASTER_SLAVE);
+	//write(CS4272_MODE_CONTROL, CS4272_MC_RATIO_SEL(3) | CS4272_MC_MASTER_SLAVE);
 
 	delay(10);
 	
@@ -217,10 +218,10 @@ bool AudioControlCS4272::write(unsigned int reg, unsigned int val)
 
 	regLocal[reg] = val;
 
-	Wire.beginTransmission(CS4272_ADDR);
-	Wire.write(reg & 0xFF);
-	Wire.write(val & 0xFF);
-	Wire.endTransmission();
+	Wire1.beginTransmission(CS4272_ADDR);
+	Wire1.write(reg & 0xFF);
+	Wire1.write(val & 0xFF);
+	Wire1.endTransmission();
 	return true;
 }
 
